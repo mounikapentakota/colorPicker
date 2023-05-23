7bc5e0db0764f408f9f883092e2675a108d2d506
mounikapentakota <131431719+mounikapentakota@users.noreply.github.com>
2023-05-23 11:07:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let colorpickercontainer=document.getElementById("colorpickercontainer");
+let SelectedColorHexCode=document.getElementById("SelectedColorHexCode");
+function colorOne(){
+    colorpickercontainer.style.backgroundColor="#e0e0e0";
+    SelectedColorHexCode.textContent="#e0e0e0"
+}
+
+function colorTwo(){
+    colorpickercontainer.style.backgroundColor="#6fcf97";
+    SelectedColorHexCode.textContent="#6fcf97"
+}
+
+function colorThree(){
+    colorpickercontainer.style.backgroundColor="#56ccf2";
+    SelectedColorHexCode.textContent="#56ccf2"
+}
+
+function colorFour(){
+    colorpickercontainer.style.backgroundColor="#bb6bd9";
+    SelectedColorHexCode.textContent="#bb6bd9"
+}
